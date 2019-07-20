c2f16f46fc109682b88c3751137bcc96c0100d65
Scott Quach <scottqglobal@gmail.com>
2019-07-20 15:16:04 -0700

    Initial setup with auth

--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios';
+import { clientId, state } from './environment';
 
 Vue.use(Vuex)
 
@@ -11,6 +13,14 @@ export default new Vuex.Store({
 
   },
   actions: {
-
+    authenticat(context) {
+      return axios.get('https://todoist.com/oauth/authorize', {
+        params: {
+          "client_id": clientId,
+          "scope": "data:read",
+          "state": state
+        }
+      })
+    }
   }
 })

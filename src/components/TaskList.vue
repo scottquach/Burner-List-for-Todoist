abe8e1ec673d70f0c913fc45c6eedbc1eac2b23c
Scott Quach <scottqglobal@gmail.com>
2019-07-27 10:26:28 -0700

    Store can now pull in all tasks and projects

---
+++ src/components/TaskList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>All Tasks</h1>
+    <div v-for="task in allTasks" :key="task.id">{{ task.content }}</div>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    allTasks: function() {
+      return this.$store.getters.allTasks;
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios';
-import { clientId, state } from './environment';
+import { clientId, state, devToken } from './environment';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    allTasks: [],
+    allProjects: []
   },
-  mutations: {
-
+  mutations: {  
+    appendAllTasks(state, newTasks) {
+      console.log(newTasks);
+      console.log(state);
+      state.allTasks.push(...newTasks);
+      console.log(state)
+    },
+    appendAllProjects(state, newProjects) {
+      state.allProjects.concat(newProjects);
+    }
+  },
+  getters: {
+    allTasks(state) {
+      return state.allTasks;
+    }
   },
   actions: {
     authenticate(context) {
@@ -24,6 +38,25 @@ export default new Vuex.Store({
         console.log(result);
       }).catch(err => {
         console.log(err);
+      })
+    },
+    fetchAllTasks(context) {
+      return axios.get('https://api.todoist.com/rest/v1/tasks', {
+        headers: {
+          "Authorization": `Bearer ${devToken}`
+        }
+      }).then(result => {
+        console.log(result);
+        context.commit("appendAllTasks", result.data)
+      })
+    },
+    fetchAllProjects(context) {
+      return axios.get('https://api.todoist.com/rest/v1/projects', {
+        headers: {
+          "Authorization": `Bearer ${devToken}`
+        }
+      }).then(result => {
+        console.log(result);
       })
     }
   }

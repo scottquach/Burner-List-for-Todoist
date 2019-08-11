f52704e683fe696458cb1fcee6d0594400c16511
Scott Quach <scottqglobal@gmail.com>
2019-08-10 20:10:48 -0700

    Modified router ordering

--- src/router.js
+++ src/router.js
@@ -12,14 +12,14 @@ const router = new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: Home
     }
   ]
 });
